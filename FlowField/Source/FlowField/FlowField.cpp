d6a5812e7c7d1f98e0c295a7a872498a65c1b96d
Simon Steele <simon.steele@mymail.champlain.edu>
2021-02-25 16:15:29 -0500

    Added default project

---
+++ FlowField/Source/FlowField/FlowField.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "FlowField.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FlowField, "FlowField" );
---
+++ FlowField/Source/FlowField/FlowFieldGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FlowFieldGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FLOWFIELD_API AFlowFieldGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
