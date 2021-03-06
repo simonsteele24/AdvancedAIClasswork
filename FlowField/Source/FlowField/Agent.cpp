ffd9923d9994165dedd48d84ab2003b1b917c428
Simon Steele <simon.steele@mymail.champlain.edu>
2021-03-06 18:59:55 -0500

    Agent can now follow flow field

--- FlowField/Source/FlowField/Agent.cpp
+++ FlowField/Source/FlowField/Agent.cpp
@@ -1,27 +1,211 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "Agent.h"
+#include "Components/StaticMeshComponent.h"
+#include "Kismet/KismetMathLibrary.h"
+#include "Components/ArrowComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "DrawDebugHelpers.h"
+#include "GridActor.h"
+#include "Kismet/GameplayStatics.h"
 
 // Sets default values
 AAgent::AAgent()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	// Agent body component
+	agentBody = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Body"));
+	agentBody->SetupAttachment(RootComponent);
+
+	// Arrow to show direction
+	directionArrow = CreateAbstractDefaultSubobject<UArrowComponent>(TEXT("Arrow"));
+	directionArrow->SetupAttachment(agentBody);
+
+	// Collision detection raycast on left
+	leftRaycast = CreateAbstractDefaultSubobject<USceneComponent>(TEXT("LeftRaycast"));
+	leftRaycast->SetupAttachment(agentBody);
+
+	// Collision detection raycast on right
+	rightRaycast = CreateAbstractDefaultSubobject<USceneComponent>(TEXT("RightRaycast"));
+	rightRaycast->SetupAttachment(agentBody);
+
+	// Collision detection raycast on left
+	leftmostRaycast = CreateAbstractDefaultSubobject<USceneComponent>(TEXT("LeftmostRaycast"));
+	leftmostRaycast->SetupAttachment(agentBody);
+
+	// Collision detection raycast on right
+	rightmostRaycast = CreateAbstractDefaultSubobject<USceneComponent>(TEXT("RightmostRaycast"));
+	rightmostRaycast->SetupAttachment(agentBody);
+
+	// Capsule collision to check for collisions
+	capsuleCollision = CreateAbstractDefaultSubobject<UCapsuleComponent>(TEXT("CapsuleCollision"));
+	capsuleCollision->SetupAttachment(agentBody);
 }
 
 // Called when the game starts or when spawned
 void AAgent::BeginPlay()
 {
 	Super::BeginPlay();
-	
+	Position = GetActorLocation();
+	startTime = GetGameTimeSinceCreation();
 }
 
 // Called every frame
 void AAgent::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
+	dt = DeltaTime;
 }
 
+
+// Moves actor to location using obstacle avoidance
+void AAgent::MoveToLocation()
+{
+	if (GetGameTimeSinceCreation() - startTime > timeOfWander)
+	{
+		startTime = GetGameTimeSinceCreation();
+	}
+
+	// steering
+	FVector thirdDimensionLocation = GetActorLocation();
+	FIntVector2D position = FIntVector2D(FMath::RoundToInt(thirdDimensionLocation.X / grid->distanceBetweenCells), FMath::RoundToInt(thirdDimensionLocation.Y / grid->distanceBetweenCells));
+	FVector2D direction = grid->GetVectorAtPosition(position);
+
+	SteeringVelocity += (SteeringVelocity * DragForce * dt);
+	SteeringVelocity += (FVector(direction.X, direction.Y, 0) * SeekStrength * dt);
+	SteeringVelocity += (Seperate() * SeperationStrength * dt);
+
+	// limit Speed
+	if (SteeringVelocity.Size() > MaxSpeed)
+	{
+		SteeringVelocity = SteeringVelocity.GetSafeNormal() * MaxSpeed;
+	}
+
+	// movement
+	Position += (SteeringVelocity * dt);
+	SetActorLocation(Position, true);
+	Position = GetActorLocation();
+
+	// orientation 
+	FRotator PlayerRot = UKismetMathLibrary::FindLookAtRotation(Position, Position + SteeringVelocity);
+	SetActorRotation(PlayerRot);
+}
+
+
+// Does the seek steering behavior
+FVector AAgent::Seek(FVector location)
+{
+	FVector dir = location - Position;
+	float distance = dir.Size();
+	dir.Z = 0; // consider only 2d plane
+	dir.Normalize();
+
+	const float maxSpeedDistance = 500.0f;
+	float speedRatio = FMath::Clamp(distance / SeekDecelerationDistance, 0.0f, 1.0f);
+
+	return dir * speedRatio;
+}
+
+
+// Seperation Behaviour
+FVector AAgent::Seperate()
+{
+	TArray<AActor*> result;
+	UGameplayStatics::GetAllActorsOfClass(GetWorld(), AAgent::StaticClass(), result);
+
+	FVector steer = FVector(0, 0, 0);
+
+	for (int i = 0; i < result.Num(); i++)
+	{
+		float dist = FVector::Distance(result[i]->GetActorLocation(), GetActorLocation());
+		if (dist < SeperationThreshold && result[i] != this)
+		{
+			float strength = (SeperationThreshold - dist);
+			FVector direction = GetActorLocation() - result[i]->GetActorLocation();
+			direction.Normalize();
+
+			steer += direction * strength;
+		}
+	}
+
+	return steer;
+}
+
+
+// Rotates point by given angle and distance around actor
+FVector AAgent::RotatePointAroundActor(float amountToRotate, float distanceOfPoint)
+{
+	FRotator rotation = GetActorRotation() + FRotator(0, 0, amountToRotate);
+
+	return GetActorLocation() + (UKismetMathLibrary::GetForwardVector(rotation) * distanceOfPoint);
+}
+
+
+
+// Updated after certain time
+void AAgent::ChangeWanderDirection()
+{
+	wanderOrientation += UKismetMathLibrary::RandomFloatInRange(-1.0f, 1.0f) * wanderRate;
+}
+
+
+// Check if collision score needs to be updated based on the number of overlaps on capsule component
+void AAgent::UpdateCollisionScore()
+{
+	TArray<AActor*> result;
+	TArray<AActor*> newResult;
+	capsuleCollision->GetOverlappingActors(result);
+	newResult.Empty();
+
+	// Take out self from overlapping actor list
+	for (int i = 0; i < result.Num(); i++)
+	{
+		if (result[i] != this)
+		{
+			newResult.Add(result[i]);
+		}
+	}
+
+
+	if (numOfOverlaps != newResult.Num() && newResult.Num() > numOfOverlaps)
+	{
+		numOfCollisions++;
+	}
+
+	numOfOverlaps = newResult.Num();
+}
+
+
+// Avoids all agents
+FVector AAgent::AvoidAgents()
+{
+	TArray<AActor*> result;
+	UGameplayStatics::GetAllActorsOfClass(GetWorld(), AAgent::StaticClass(), result);
+
+
+	for (int i = 0; i < result.Num(); i++)
+	{
+		if (result[i] != this)
+		{
+			FVector direction = GetActorLocation() - result[i]->GetActorLocation();
+
+			if (direction.Size() <= agentCollisionRadius)
+			{
+				direction.Normalize();
+				return direction;
+			}
+		}
+	}
+
+	return FVector(0, 0, 0);
+}
+
+// Gets and sets the grid
+void AAgent::InitializeGrid() 
+{
+	TArray<AActor*> result;
+	UGameplayStatics::GetAllActorsOfClass(GetWorld(), AGridActor::StaticClass(), result);
+	grid = Cast<AGridActor>(result[0]);
+}
--- FlowField/Source/FlowField/Agent.h
+++ FlowField/Source/FlowField/Agent.h
@@ -1,5 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
@@ -10,16 +8,81 @@ UCLASS()
 class FLOWFIELD_API AAgent : public AActor
 {
 	GENERATED_BODY()
-	
-public:	
+
+		/* Components */
+		UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true")) class UStaticMeshComponent* agentBody;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true")) class UArrowComponent* directionArrow;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true")) class USceneComponent* leftRaycast;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true")) class USceneComponent* rightRaycast;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true")) class USceneComponent* leftmostRaycast;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true")) class USceneComponent* rightmostRaycast;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true")) class UCapsuleComponent* capsuleCollision;
+
+
+public:
 	// Sets default values for this actor's properties
 	AAgent();
+
+	// Functions
+	UFUNCTION(BlueprintCallable) void MoveToLocation();
+	UFUNCTION(BlueprintCallable) void UpdateCollisionScore();
+	UFUNCTION(BlueprintCallable) void InitializeGrid();
+
+	UPROPERTY(BlueprintReadOnly) FVector SteeringVelocity = FVector(0, 0, 0);
+	UPROPERTY(BlueprintReadOnly) int numOfCollisions = 0;
 
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
+	// Floats
+	UPROPERTY() float dt;
+	UPROPERTY() bool bObjectInWay;
+	UPROPERTY() int numOfOverlaps = 0;
+	UPROPERTY() float wanderOffset = 0.0f;
+	UPROPERTY() float wanderRadius = 0.0f;
+	UPROPERTY(BlueprintReadWrite) float wanderOrientation = 0.0f;
+	UPROPERTY() float startTime = 0.0f;
+
+	// Vectors
+	UPROPERTY() FVector locationToMoveTo;
+
+	// Private Functions
+	UFUNCTION() void ChangeWanderDirection();
+	UFUNCTION() FVector Seek(FVector location);
+	UFUNCTION() FVector Seperate();
+	UFUNCTION() FVector AvoidAgents();
+	UFUNCTION() FVector RotatePointAroundActor(float amountToRotate, float distanceOfPoint);
+
+	// Grid Actors
+	UPROPERTY() class AGridActor* grid;
+
+	FVector Position = FVector(0, 0, 0);
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Wander") float wanderStrength = 900.0f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Wander") float wanderRate = 1.0f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Wander") float circleOffset = 1.0f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Wander") float timeOfWander = 1.0f;
+
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Steering") float MaxSpeed = 700.0f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Steering") float DragForce = -0.8f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Steering") float SeekStrength = 900.0f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Steering") float SeekDecelerationDistance = 1200.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Seperation") float SeperationStrength = 1000.0f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Seperation") float SeperationThreshold = 100.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "CollisionAvoidance") float coneThreshold = 30.0f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "CollisionAvoidance") float coneDistance = 500.0f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "CollisionAvoidance") float endDistance = 500.0f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "CollisionAvoidance") float avoidStrength = 500.0f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "CollisionAvoidance") float avoidDistance = 50.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AgentAvoidance") float agentAvoidanceStrength = 500.0f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AgentAvoidance") float agentCollisionRadius = 100.0f;
+
+public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
