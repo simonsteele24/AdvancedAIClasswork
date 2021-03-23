4d6fa7de8a8d62b86bd8726160a1158c3f0442ce
Simon Steele <simon.steele@mymail.champlain.edu>
2021-03-23 14:20:00 -0400

    Added initial project for influence map

---
+++ InfluenceMap/Source/InfluenceMap/InfluenceMapGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "InfluenceMapGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class INFLUENCEMAP_API AInfluenceMapGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
